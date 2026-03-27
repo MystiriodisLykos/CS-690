31c8fac3a93709e666b6f2f506fd1e3bb86d8d7b
Brendan DeLeeuw <13935754+MystiriodisLykos@users.noreply.github.com>
2026-03-27 04:24:00 +0000

    Trying to make a thing for editing notes but it is just not working Maybe not possible in codespaces?

--- event-planner/EventPlanner-cli/NoteMenu.cs
+++ event-planner/EventPlanner-cli/NoteMenu.cs
@@ -3,16 +3,44 @@ namespace EvenPlannerCLI;
 using Spectre.Console;
 using PlannerService;
 using Persistence = PlannerService.Storage;
+using System.Diagnostics;
 
 class NoteMenu : INestedMenu {
     public string MenuName { get; } = "Notes";
 
     protected static MenuOfMenus Menu = new(
-        new List<INestedMenu> {new ShowAllNotes()},
+        new List<INestedMenu> {new EditNote(), new ShowAllNotes()},
         "What would you like to do?"
     );
     public void Run(Event Event) {
         Menu.Run(Event);
+    }
+}
+
+class EditNote : INestedMenu
+{
+    public string MenuName { get; } = "Edit Note";
+
+    public void Run(Event Event)
+    {
+        var notes = Event.Notes.Concat(Event.GetAllGuestNotes());
+
+        if (! notes.Any())
+        {
+            AnsiConsole.Confirm("Must have at least one Note before it can be editted. (Enter to Continue)");
+            return;
+        }
+        var note = AnsiConsole.Prompt(
+            new SelectionPrompt<Note>()
+            .Title("Select Note to Edit")
+            .WrapAround()
+            .AddChoices(notes)
+            .UseConverter(option => Persistence.Notes.ReadNote(option))
+        );
+
+        Persistence.Notes.EditNote(note);
+
+        Console.WriteLine("Done");
     }
 }
 
