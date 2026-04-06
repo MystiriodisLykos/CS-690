7c1303b3805a244f3d276c2b09f01add05330edd
Brendan DeLeeuw <13935754+MystiriodisLykos@users.noreply.github.com>
2026-04-05 23:23:02 -0600

    framework for testing the cli

---
+++ event-planner/EventPlanner-cli.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace EventPlanner_cli.Test;
+
+
+using Spectre.Console;
+using StorageService;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+	AnsiConsole.QueueResponse("hello");
+	Storage.SetEditCallback(x => 'a');
+    }
+}
